ea83f9c57f04be7795dc0c6333f1f763cb159e45
Anton <35538867+Anton13602@users.noreply.github.com>
2024-01-30 00:27:50 +0300

    feat: add worker

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,38 @@
+import { Worker } from 'node:worker_threads';
+import { cpus } from 'os';
+
+import { getPath } from '../utils/getPath.js';
+
 const performCalculations = async () => {
-    // Write your code here
+	const result = [];
+	const cpu = cpus();
+
+	const pathToWorker = getPath(import.meta, ['worker.js']);
+
+	const promises = cpu.map((value, i) => {
+		return new Promise((resolve, reject) => {
+			const worker = new Worker(pathToWorker, {
+				workerData: 10 + i,
+			});
+
+			worker.on('message', (value) => {
+				resolve(value);
+			});
+			worker.on('error', () => {
+				reject();
+			});
+		});
+	});
+
+	const res = await Promise.allSettled(promises)
+	res.forEach(({ status, value }) => {
+		result.push({
+			'status': status === 'fulfilled' ? 'resolved' : 'error',
+			'data': status === 'fulfilled' ? value: null,
+		})
+	})
+
+	console.log(result);
 };
 
 await performCalculations();
