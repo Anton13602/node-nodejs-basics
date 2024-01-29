a8a28f757b6410e04126baccf21e9cfdb3a69b62
Anton <35538867+Anton13602@users.noreply.github.com>
2024-01-30 01:27:24 +0300

    feat: add cp

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,11 @@
+import { fork } from 'node:child_process';
+import { getPath } from '../utils/getPath.js';
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+	// Write your code here
+	const path = getPath(import.meta, ['files', 'script.js']);
+	fork(path, args);
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess(['--someArgument1']);
