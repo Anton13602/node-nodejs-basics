16cd5776bcdd253dfd59ac9fb237e01c5d2b97c0
Anton <35538867+Anton13602@users.noreply.github.com>
2024-01-29 21:58:32 +0300

    feat: implement streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+import { createReadStream } from 'fs';
+import { stdout } from 'node:process';
+
+import { getPath } from '../utils/getPath.js';
+
 const read = async () => {
-    // Write your code here 
+	// Write your code here
+	const pathToFile = getPath(import.meta, ['files', 'fileToRead.txt']);
+	const readStream = createReadStream(pathToFile);
+
+	// readStream.pipe(stdout);
+
+	readStream.on('data', (chunk) => {
+		stdout.write(chunk.toString());
+	});
 };
 
 await read();
--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,21 @@
+import { Transform } from 'stream';
+import { stdin, stdout } from 'node:process';
+
 const transform = async () => {
-    // Write your code here 
+	// Write your code here
+	const reverseString = new Transform({
+		transform(chunk, encoding, callback) {
+			callback(null, chunk.toString().split('').reverse().join(''));
+		},
+	});
+
+	stdin.on('data', (chunk) => {
+		reverseString.on('data', (chunkReverse) => {
+			stdout.write(chunkReverse.toString());
+		});
+
+		reverseString.write(chunk);
+	});
 };
 
 await transform();
